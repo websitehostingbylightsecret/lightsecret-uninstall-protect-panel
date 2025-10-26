ed276ee106a8c63fe03211d8f9d4ad0b4aeb3ffe
websitehostingbylightsecret <websitelightsecret@gmail.com>
2025-10-26 14:18:36 +0700

    Add files via upload

---
+++ lightsecretprotect14.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+REMOTE_PATH="/var/www/pterodactyl/app/Http/Controllers/Admin/Servers/ServerTransferController.php"
+TIMESTAMP=$(date -u +"%Y-%m-%d-%H-%M-%S")
+
+echo "🚀 Uninstall Proteksi Anti Modifikasi Server 3..."
+
+# Pastikan folder tujuan ada
+mkdir -p "$(dirname "$REMOTE_PATH")"
+chmod 755 "$(dirname "$REMOTE_PATH")"
+
+# Tulis ulang file baru
+cat > "$REMOTE_PATH" <<'EOF'
+<?php
+
+namespace Pterodactyl\Http\Controllers\Admin\Servers;
+
+use Illuminate\View\View;
+use Illuminate\Http\Request;
+use Pterodactyl\Models\Server;
+use Spatie\QueryBuilder\QueryBuilder;
+use Spatie\QueryBuilder\AllowedFilter;
+use Pterodactyl\Http\Controllers\Controller;
+use Pterodactyl\Models\Filters\AdminServerFilter;
+use Illuminate\Contracts\View\Factory as ViewFactory;
+
+class ServerController extends Controller
+{
+    /**
+     * ServerController constructor.
+     */
+    public function __construct(private ViewFactory $view)
+    {
+    }
+
+    /**
+     * Returns all the servers that exist on the system using a paginated result set. If
+     * a query is passed along in the request it is also passed to the repository function.
+     */
+    public function index(Request $request): View
+    {
+        $servers = QueryBuilder::for(Server::query()->with('node', 'user', 'allocation'))
+            ->allowedFilters([
+                AllowedFilter::exact('owner_id'),
+                AllowedFilter::custom('*', new AdminServerFilter()),
+            ])
+            ->paginate(config()->get('pterodactyl.paginate.admin.servers'));
+
+        return $this->view->make('admin.servers.index', ['servers' => $servers]);
+    }
+}
+
+EOF
+
+# Atur permission file
+chmod 644 "$REMOTE_PATH"
+echo "✅ Uninstall Proteksi Anti Modifikasi Server 3 berhasil dipasang!"
+echo "📂 Lokasi file: $REMOTE_PATH"
